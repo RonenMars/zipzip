410fa299817cc635b58327178efa198f40a80789
RonenMars <cleanclaw@gmail.com>
2023-07-25 05:25:13 +0300

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
-import { Login } from '@components/pages';
+import { Link } from 'react-router-dom';
+import Main from '@components/templates/Login';
 
 function App() {
   return (
     <>
-      <Login />
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/topics">Topics</Link>
+        </li>
+        <li>
+          <Link to="/settings">Settings</Link>
+        </li>
+      </ul>
+      <hr />
+      <Main />
     </>
   );
 }
