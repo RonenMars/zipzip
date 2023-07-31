d4489abd202270b88afb10062d696c4ba3403da9
RonenMars <cleanclaw@gmail.com>
2023-07-31 10:00:56 +0300

    Change main component height

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { LoginPhone } from '@components/pages';
 function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <div className="bg-white h-2/6 w-96 rounded-3xl shadow-lg pt-8 px-14">
+      <div className="bg-white w-96 rounded-3xl shadow-lg py-8 px-14">
         <LoginPhone />
       </div>
     </div>
