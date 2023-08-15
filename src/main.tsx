2ad2184c19ba5b448467be205bf66f3c408a8fe0
serge <j0nybr4v0@gmail.com>
2023-08-15 14:55:11 +0300

    fix build

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import ReactDOM from 'react-dom/client';import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import './i18n';
