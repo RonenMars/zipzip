f807811238c735c6c163f641875ee72234046312
RonenMars <cleanclaw@gmail.com>
2023-08-20 20:11:29 +0300

    Add phone local validation

--- src/validations/user/login/phone.schema.ts
+++ src/validations/user/login/phone.schema.ts
@@ -1,10 +1,18 @@
 import * as Joi from 'joi';
+import { isValidPhoneNumber } from 'libphonenumber-js';
 
 export const LoginPhoneSchema = Joi.object({
   phone: Joi.string()
     .pattern(/^[0-9]+$/)
     .length(10)
     .required()
+    .custom((value, helper) => {
+      if (!isValidPhoneNumber(value, 'IL')) {
+        return helper.message('user.validation.phone.isLocalValid' as unknown as Joi.LanguageMessages);
+      }
+
+      return true;
+    })
     .messages({
       'string.length': 'user.validation.phone.length',
       'string.pattern.base': 'user.validation.phone.pattern',
