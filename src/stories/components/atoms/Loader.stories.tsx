466afe1d146b7d09bcdf5d30b8f02b896664b6ed
RonenMars <cleanclaw@gmail.com>
2023-09-16 14:55:24 +0300

    Add Loader component to storybook and refine other storybook components

---
+++ src/stories/components/atoms/Loader.stories.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import Loader from '@components/atoms/loader/Loader';
+import { Provider } from 'react-redux';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { LoaderInitialState } from '@redux/LoaderReducer.ts';
+import { StoryFn } from '@storybook/react';
+
+export const MockedState = {
+  loading: true,
+};
+
+// A super-simple mock of a redux store
+const Mockstore = ({ loaderState, children }: { loaderState: LoaderInitialState; children: React.ReactNode }) => (
+  <Provider
+    store={configureStore({
+      reducer: {
+        loader: createSlice({
+          name: 'loader',
+          initialState: loaderState,
+          reducers: {},
+        }).reducer,
+      },
+    })}
+  >
+    {children}
+  </Provider>
+);
+
+export default {
+  component: Loader,
+  title: 'Example/Loader',
+  decorators: [(Story: StoryFn) => <Story />],
+  tags: ['autodocs'],
+  excludeStories: /.*MockedState$/,
+};
+
+export const Default = {
+  decorators: [
+    (Story: StoryFn) => (
+      <Mockstore loaderState={MockedState}>
+        <Story />
+      </Mockstore>
+    ),
+  ],
+};
+export const WithBackground = {
+  decorators: [
+    (Story: StoryFn) => (
+      <Mockstore loaderState={MockedState}>
+        <div className="bg-black w-screen h-screen">
+          <Story />
+        </div>
+      </Mockstore>
+    ),
+  ],
+};
