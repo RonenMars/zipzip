e5957c2a233277800a150d129b326a766e6a2f72
RonenMars <ronenmars@gmail.com>
2023-11-10 15:27:21 +0200

    Center app menu

--- src/components/molecules/menu/Menu.tsx
+++ src/components/molecules/menu/Menu.tsx
@@ -33,7 +33,7 @@ export const Menu = ({ open, setOpen }: MenuInterface) => {
       <motion.div animate={{ opacity: 1 }} initial={{ opacity: 0 }}>
         {open ? (
           <div className="absolute w-full h-full bg-purple-100 opacity-90 z-30 top-0 right-0">
-            <div className="absolute top-1/3 right-1/2 transform translate-x-1/2 translate-y-1/2">
+            <div className="absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 top-1/2">
               {menuItems.map(({ name, menuItemAction }, index) => (
                 <MenuItem
                   displaySeparator={index < menuItems.length - 1}
